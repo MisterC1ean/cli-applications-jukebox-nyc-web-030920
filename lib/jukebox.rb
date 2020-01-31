c51a60decaf661253103e82756e494bdde43f944
Robert Keller <robert_keller44@icloud.com>
2020-01-31 19:45:00 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -36,9 +36,10 @@ def play(songs)
   # if track number could exist as index of our array
   if(1..9).to_a.include?(input.to_i)
     puts "Playing #{songs[input.to_i - 1]}"
-    
+  
   elsif songs.index(input) != nil
     puts "Playing #{input}"
+    
   else
     puts "Invalid input, please try again"
 end
